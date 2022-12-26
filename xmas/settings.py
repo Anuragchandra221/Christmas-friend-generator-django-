e9309c5b95d05654b3b44477194bc309b67ba20e
AnuragChandra221 <pothaluranurag2002@gmail.com>
2022-12-27 01:00:31 +0530

    updated allowed hosts

--- xmas/settings.py
+++ xmas/settings.py
@@ -153,5 +153,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:3000",
-    "https://bemyfriend.surge.sh/",
+    "https://bemyfriend.surge.sh",
 ]
