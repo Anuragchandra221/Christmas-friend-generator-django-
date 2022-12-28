4d92a68a75062f2d66101c81fd3965600e030be9
AnuragChandra221 <pothaluranurag2002@gmail.com>
2022-12-28 16:39:06 +0530

    more secured

--- xmas/settings.py
+++ xmas/settings.py
@@ -149,7 +149,7 @@ STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # CORS_ORIGIN_WHITELIST = "http://localhost:3000",
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = False
 # CORS_ALLOWED_ORIGIN = [
 #     'http://127.0.0.1:3000',
 #     "http://localhost:3000",
